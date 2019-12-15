ac6277bf614d85d7857b9cef56538741c9668b77
GustavoAcevedoUnal <51792529+GustavoAAA@users.noreply.github.com>
2019-12-15 17:17:38 -0500

    Punto 3

--- 01-hadoop-50/q03-10/mapper.py
+++ 01-hadoop-50/q03-10/mapper.py
@@ -1,4 +1,9 @@
+#! /usr/bin/env python
+
 import sys
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+if __name__ == "__main__":
+    for line in sys.stdin:
+      line = line.strip()
+      key_columna = line.split(',')[1]
+      valor_columna = line.split(',')[0]
+      sys.stdout.write("{},{}\n".format(key_columna,valor_columna))
