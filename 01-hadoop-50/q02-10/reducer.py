30f8de5a16aaef5c282fe1ba6c47eafb8e4815c7
GustavoAcevedoUnal <51792529+GustavoAAA@users.noreply.github.com>
2019-12-15 14:00:14 -0500

    Punto 1 y 2 Hadoop

--- 01-hadoop-50/q02-10/reducer.py
+++ 01-hadoop-50/q02-10/reducer.py
@@ -1,4 +1,27 @@
+#!/usr/bin/env python
+
 import sys
-#
-#  >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+
+if __name__ == '__main__':
+
+    curkey = None
+    total = None
+
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = int(val)
+        
+        if total is None:
+          total = val
+
+        if key == curkey:
+              total = max(total,val)
+        else:
+            
+            if curkey is not None:
+               
+                sys.stdout.write("{}\t{}\n".format(curkey, total))
+
+            curkey = key
+            total = val
+    sys.stdout.write("{}\t{}\n".format(curkey, total))
